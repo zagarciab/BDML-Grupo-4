efc5ab34d773e2db2a624533277fc28280461bba
Alejandro Sarasti Sierra <54252892+sarasti2@users.noreply.github.com>
2025-01-29 19:33:54 -0500

    Update model3.R

--- model3.R
+++ model3.R
@@ -1,1 +1,12 @@
 # Fill here the code to estimate the linear_model3 from the notebook
+db_int <- db_int %>% filter(m1_std_residuals<2 & m1_std_residuals>-2 )
+linear_model3<- lm(
+  totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender
+                                 + H_Head + age + gender,
+  data=db_int)
+
+
+
+stargazer(linear_model, linear_model2, linear_model3  , type="text",
+          covariate.labels=c("Mean Ocu Income","N under 18","Male",
+                             "Hausehold Head","Age", "N under 18 x Male" ))
