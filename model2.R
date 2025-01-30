226ad8dec3f27160174cde9d2d8d14489349487d
CamiloRamirezAs <ca.ramirez10@uniandes.edu.co>
2025-01-29 19:23:52 -0500

    Update model2.R

--- model2.R
+++ model2.R
@@ -1,2 +1,10 @@
 
 # Fill here the code to estimate the linear_model2 from the notebook
+
+# re run the model
+linear_model2<- lm(totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender + H_Head + age + gender, data=db_int2  )
+
+
+stargazer(linear_model, linear_model2, type="text",
+          covariate.labels=c("Mean Ocu Income","N under 18","Male",
+                             "Hausehold Head","Age", "N under 18 x Male" ))
